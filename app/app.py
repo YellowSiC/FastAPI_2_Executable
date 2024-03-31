81f54b3b09e6cd02c61e554cbb6d32336af7cb24
Ari Bermeki <90009669+AriBermeki@users.noreply.github.com>
2024-03-31 04:26:00 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
+import os
+import sys
 from pathlib import Path
 
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, Response
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -21,26 +23,59 @@ async def read_item(request: Request):
         "X-Forwarded-Prefix", request.scope.get("root_path", "")
     )
     return templates.TemplateResponse(
-        request=request, name="index.html", context={"prefix": prefix}
+        "index.html", {"request": request, "prefix": prefix}
     )
 
 
 @app.get("/favicon.ico", response_class=HTMLResponse)
 async def favicon_ico(request: Request):
-    pass
+    return Response(content=b"", media_type="image/x-icon")
 
 
 @app.get("/logging", response_class=HTMLResponse)
 async def uvicorn_logging(request: Request):
-    log_path = Path(__file__).parent / "../server.log"
-    with open(log_path, "rb") as log:
-        log_read = log.read()
-        return log_read
+    try:
+        if getattr(sys, "frozen", False):
+            application_path = os.path.dirname(sys.executable)
+        else:
+            application_path = os.path.dirname(__file__)
+        log_file_path = os.path.join(application_path, "Serverlog", "server.log")
+        if os.path.exists(log_file_path):
+            with open(log_file_path, "rb") as log:
+                log_read = log.read()
+                return Response(content=log_read, media_type="text/plain")
+        else:
+            return Response(
+                content="Die Serverlog-Datei wurde nicht gefunden.",
+                media_type="text/plain",
+            )
+    except Exception as e:
+        return Response(content=str(e), media_type="text/plain")
 
 
 @app.get("/remove_logging")
 async def remove_logging(request: Request):
-    log_path = Path(__file__).parent / "../server.log"
-    with open(log_path, "w"):
-        pass
-    return Response(content="Inhalt der Datei wurde gelöscht.", media_type="text/plain")
+    try:
+        if getattr(sys, "frozen", False):
+            application_path = os.path.dirname(sys.executable)
+        else:
+            application_path = os.path.dirname(__file__)
+        log_file_path = os.path.join(application_path, "Serverlog", "server.log")
+        if os.path.exists(log_file_path):
+            with open(log_file_path, "w"):
+                pass
+        else:
+            return Response(
+                content="Die Serverlog-Datei wurde nicht gefunden.",
+                media_type="text/plain",
+            )
+    except Exception as e:
+        return Response(content=str(e), media_type="text/plain")
+
+
+@app.get("/exit")
+async def server_exit(request: Request):
+    import os
+    import signal
+
+    os.kill(os.getpid(), signal.SIGINT)
